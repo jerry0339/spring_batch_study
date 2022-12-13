79da2cd18ef0677f1d49490f51bca0b2bf9cda85
Jeon Chanui <jerry0339@nhn.com>
2022-12-13 15:22:12 +0900

    test: feature ttt squash merge test

---
+++ src/main/java/io/springbatch/springbatchlecture/FeatureTttTest.java
@@ -0,0 +1,14 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class FeatureTttTest {
+	// test1
+	// test2
+	// test3
+
+	// test1 after squash
+	// test2 after squash and ttt2Test
+	// test3 after squash and ttt2Test
+}
